1851c1dadedb1ae23b67cac63e24979f72245334
UDUMULA SIVA SANKAR REDDY <96323993+USIVASANKARREDDY@users.noreply.github.com>
2022-01-21 11:28:12 +0530

    #include<stdio.h> int main() { int num1,num2,temp1,temp2,gcd,lcm,x; scanf("%d",&num1); scanf("%d",&num2);
    temp1=num1;
    temp2=num2;
    
    while(temp2!=0)
    {
    x=temp2;
    temp2=temp1%temp2;
    temp1=x;
    }
    
    gcd=temp1;
    lcm=(num1*num2)/gcd;
    printf("%d
    ",lcm);
    }

---
+++ LCM.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,temp1,temp2,gcd,lcm,x;
+scanf("%d",&num1);
+scanf("%d",&num2);
+
+temp1=num1;
+temp2=num2;
+
+while(temp2!=0)
+{
+x=temp2;
+temp2=temp1%temp2;
+temp1=x;
+}
+
+gcd=temp1;
+lcm=(num1*num2)/gcd;
+printf("%d
+",lcm);
+}
