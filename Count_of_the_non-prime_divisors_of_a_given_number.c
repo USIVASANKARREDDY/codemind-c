5aad2c16c2d7d56540818e8bcbfd976265811a8d
UDUMULA SIVA SANKAR REDDY <96323993+USIVASANKARREDDY@users.noreply.github.com>
2022-03-06 14:51:05 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,j,c=1;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             for(j=2;j<=sqrt(i);j++)             {                 if(i%j==0)                 {                     c++;                     break;                 }             }         }     }     printf("%d",c); }

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,j,c=1;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            for(j=2;j<=sqrt(i);j++)
+            {
+                if(i%j==0)
+                {
+                    c++;
+                    break;
+                }
+            }
+        }
+    }
+    printf("%d",c);
+}
